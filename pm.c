497b7466dda4afe977de042fc97c047519c929e5
Ad Nunes Ribeiro <adnunesribeiro@gmail.com>
2018-09-11 17:46:31 -0300

    Add files via upload

---
+++ pm.c
@@ -0,0 +1,243 @@
+/*
+ * Copyright(C) 2011-2016 Pedro H. Penna   <pedrohenriquepenna@gmail.com>
+ *              2015-2016 Davidson Francis <davidsondfgl@hotmail.com>
+ *              2016-2016 Subhra S. Sarkar <rurtle.coder@gmail.com>
+ *
+ * This file is part of Nanvix.
+ *
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <nanvix/config.h>
+#include <nanvix/const.h>
+#include <nanvix/dev.h>
+#include <nanvix/fs.h>
+#include <nanvix/hal.h>
+#include <nanvix/mm.h>
+#include <nanvix/pm.h>
+#include <nanvix/klib.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <limits.h>
+
+#define AGING_CONST -5;
+/**
+ * @brief Idle process page directory.
+ */
+EXTERN struct pde idle_pgdir[];
+
+/**
+ * @brief Idle process kernel stack.
+ */
+PUBLIC char idle_kstack[KSTACK_SIZE];
+
+/**
+ * @brief Process table.
+ */
+PUBLIC struct process proctab[PROC_MAX];
+
+/**
+ * @brief Current running process.
+ */
+PUBLIC struct process *curr_proc = IDLE;
+
+/**
+ * @brief Last running process.
+ */
+PUBLIC struct process *last_proc = IDLE;
+
+/**
+ * @brief Next available process ID.
+ */
+PUBLIC pid_t next_pid = 0;
+
+/**
+ * @brief Current number of processes in the system.
+ */
+PUBLIC unsigned nprocs = 0;
+
+/**
+ * @brief Element of a Queue
+ */
+ EXTERN struct process_queue{
+    struct process processo;
+    int queue_priority;
+};
+
+/**
+ * @brief Queue struct.
+ */
+ EXTERN struct Queue
+ {
+     int front, rear, size;
+     unsigned capacity;
+     struct process_queue array[PROC_MAX];
+ };
+
+ // function to create a queue of given capacity.
+// It initializes size of queue as 0
+PUBLIC struct Queue* createQueue()
+{
+    struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue));
+    queue->capacity = PROC_MAX;
+    queue->front = queue->size = 0;
+    queue->rear = PROC_MAX - 1;  // This is important, see the enqueue
+    return queue;
+}
+
+// Queue is full when size becomes equal to the capacity
+PUBLIC int isFull(struct Queue* queue)
+{  return (queue->size == queue->capacity);  }
+
+// Queue is empty when size is 0
+PUBLIC int isEmpty(struct Queue* queue)
+{  return (queue->size == 0); }
+
+// Function to add an item to the queue.
+// It changes rear and size
+PUBLIC void enqueue(struct Queue* queue,struct  process_queue item)
+{
+    if (isFull(queue))
+        return;
+    queue->rear = (queue->rear + 1)%queue->capacity;
+    queue->array[queue->rear] = item;
+    queue->size = queue->size + 1;
+    //printf("%d enqueued to queue\n", item);
+}
+
+// Function to remove an item from queue.
+// It changes front and size
+PUBLIC struct process_queue dequeue(struct Queue* queue)
+{
+    if (isEmpty(queue))
+        return;
+    struct process_queue item = queue->array[queue->front];
+    queue->front = (queue->front + 1)%queue->capacity;
+    queue->size = queue->size - 1;
+    return item;
+}
+
+//Funcao para incrementar as prioridades
+PUBLIC void aging(struct Queue* queue){
+    process_queue pq;
+    if (isEmpty(queue))
+        return;
+
+    for (int i = 0; i < queue->size; i++) {
+      /* code */
+      array[i].queue_priority += AGING_CONST;
+
+      //limits
+      if(array[i].queue_priority + array[i].process.priority <= -100){
+        array[i].queue_priority = -100 - array[i].process.priority;
+      }
+      else if(array[i].queue_priority + array[i].process.priority <= 200){
+        array[i].queue_priority = 200 - array[i].process.priority;
+      }
+    }
+
+}
+// Function to get front of queue
+PUBLIC struct process_queue front(struct Queue* queue)
+{
+    if (isEmpty(queue))
+        return;
+    return queue->array[queue->front];
+}
+
+// Function to get rear of queue
+PUBLIC struct process_queue rear(struct Queue* queue)
+{
+    if (isEmpty(queue))
+        return;
+    return queue->array[queue->rear];
+}
+
+//Process queue
+
+PUBLIC struct Queue f0;
+PUBLIC struct Queue f1;
+PUBLIC struct Queue f2;
+PUBLIC struct Queue f3;
+PUBLIC struct Queue f4;
+//PUBLIC struct Queue sleeping_queue;
+
+PUBLIC int curr_prio = 0;
+/**
+ * @brief Initializes the process management system.
+ */
+PUBLIC void pm_init(void)
+{
+	int i;             /* Loop index.      */
+	struct process *p; /* Working process. */
+
+	/* Initialize the process table.
+	for (p = FIRST_PROC; p <= LAST_PROC; p++)
+		p->flags = 0, p->state = PROC_DEAD;
+  */
+  /*Initialize the process queues.*/
+
+  f0 = createQueue(PROC_MAX);
+  f1 = createQueue(PROC_MAX);
+  f2 = createQueue(PROC_MAX);
+  f3 = createQueue(PROC_MAX);
+  f4 = createQueue(PROC_MAX);
+
+	/* Handcraft init process. */
+	IDLE->cr3 = (dword_t)idle_pgdir;
+	IDLE->intlvl = 1;
+	IDLE->flags = 0;
+	IDLE->received = 0;
+	IDLE->kstack = idle_kstack;
+	IDLE->restorer = NULL;
+	for (i = 0; i < NR_SIGNALS; i++)
+		IDLE->handlers[i] = SIG_DFL;
+	IDLE->irqlvl = INT_LVL_5;
+	IDLE->pgdir = idle_pgdir;
+	for (i = 0; i < NR_PREGIONS; i++)
+		IDLE->pregs[i].reg = NULL;
+	IDLE->size = 0;
+	for (i = 0; i < OPEN_MAX; i++)
+		IDLE->ofiles[i] = NULL;
+	IDLE->close = 0;
+	IDLE->umask = S_IXUSR | S_IWGRP | S_IXGRP | S_IWOTH | S_IXOTH;
+	IDLE->tty = NULL_DEV;
+	IDLE->status = 0;
+	IDLE->nchildren = 0;
+	IDLE->uid = SUPERUSER;
+	IDLE->euid = SUPERUSER;
+	IDLE->suid = SUPERUSER;
+	IDLE->gid = SUPERGROUP;
+	IDLE->egid = SUPERGROUP;
+	IDLE->sgid = SUPERGROUP;
+	IDLE->pid = next_pid++;
+	IDLE->pgrp = IDLE;
+	IDLE->father = NULL;
+	kstrncpy(IDLE->name, "idle", NAME_MAX);
+	IDLE->utime = 0;
+	IDLE->ktime = 0;
+	IDLE->cutime = 0;
+	IDLE->cktime = 0;
+	IDLE->state = PROC_RUNNING;
+	IDLE->counter = PROC_QUANTUM;
+	IDLE->priority = PRIO_USER;
+	IDLE->nice = NZERO;
+	IDLE->alarm = 0;
+	IDLE->next = NULL;
+	IDLE->chain = NULL;
+
+	nprocs++;
+
+	enable_interrupts();
+}
---
+++ sched.c
@@ -0,0 +1,182 @@
+/*
+ * Copyright(C) 2011-2016 Pedro H. Penna   <pedrohenriquepenna@gmail.com>
+ *              2015-2016 Davidson Francis <davidsondfgl@hotmail.com>
+ *
+ * This file is part of Nanvix.
+ *
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <nanvix/clock.h>
+#include <nanvix/const.h>
+#include <nanvix/hal.h>
+#include <nanvix/pm.h>
+#include <signal.h>
+
+/**
+ * @brief Schedules a process to execution.
+ *
+ * @param proc Process to be scheduled.
+ */
+PUBLIC void sched(struct process *proc, int queue_p)
+{
+	proc->state = PROC_READY;
+	proc->counter = 0;
+
+	struct process_queue *p;
+	p->process = proc;
+	p->queue_priority = queue_p;
+
+	if(proc-> priority + queue_p >= 119) {
+   /* f4 */
+	 enqueue(f4, p);
+ } else if( proc-> priority + queue_p >= 80) {
+   /* f3 */
+	 enqueue(f3, p);
+ } else if( proc-> priority + queue_p >= PRIO_USER) {
+   /* f2 */
+	 enqueue(f2, p);
+ } else if( proc-> priority + queue_p >= PRIO_REGION) {
+	 /* f1 */
+	 enqueue(f1, p);
+	} else {
+   /*f0*/
+	 enqueue(f0, p);
+	}
+}
+
+/**
+ * @brief Stops the current running process.
+ */
+PUBLIC void stop(void)
+{
+	curr_proc->state = PROC_STOPPED;
+	sndsig(curr_proc->father, SIGCHLD);
+	yield();
+}
+
+/**
+ * @brief Resumes a process.
+ *
+ * @param proc Process to be resumed.
+ *
+ * @note The process must stopped to be resumed.
+ */
+PUBLIC void resume(struct process *proc)
+{
+	/* Resume only if process has stopped. */
+	if (proc->state == PROC_STOPPED)
+		sched(proc, int x);
+}
+
+/**
+ * @brief Yields the processor.
+ */
+PUBLIC void yield(void)
+{
+	struct process *p;    /* Working process.     */
+	struct process *next; /* Next process to run. */
+	struct process_queue *processo_queue;
+
+	/* Re-schedule process for execution. */
+	if (curr_proc->state == PROC_RUNNING)
+		sched(curr_proc,curr_prio + 50);
+
+	/* Remember this process. */
+	last_proc = curr_proc;
+
+
+	/* Check alarm. */
+	for (p = FIRST_PROC; p <= LAST_PROC; p++)
+	{
+		/* Skip invalid processes. */
+		if (!IS_VALID(p))
+			continue;
+
+		/* Alarm has expired. */
+		if ((p->alarm) && (p->alarm < ticks))
+			p->alarm = 0, sndsig(p, SIGALRM);
+	}
+
+	/* Choose a process to run next. */
+	next = IDLE;
+
+	if(isEmpty(!f0)) {
+		processo_queue = dequeue(f0);
+		next = processo_queue.processo;
+		curr_prio = processo_queue.queue_priority;
+
+		//ageing
+		aging(f1);
+		aging(f2);
+		aging(f3);
+		aging(f4);
+
+ } else if(!isEmpty(f1)) {
+	 processo_queue = dequeue(f1);
+	 next = processo_queue.processo;
+	 curr_prio = processo_queue.queue_priority;
+
+	 aging(f2);
+	 aging(f3);
+	 aging(f4);
+
+ } else if(!isEmpty(f2)) {
+	 processo_queue = dequeue(f2);
+	 next = processo_queue.processo;
+	 curr_prio = processo_queue.queue_priority;
+
+	 aging(f3);
+	 aging(f4);
+ } else if(!isEmpty(f3)) {
+	 processo_queue = dequeue(f3);
+	 next = processo_queue.processo;
+	 curr_prio = processo_queue.queue_priority;
+
+	 aging(f4);
+ } else if(!isEmpty(f4)){
+	 processo_queue = dequeue(f4);
+	 next = processo_queue.processo;
+	 curr_prio = processo_queue.queue_priority;
+	}
+	// for (p = FIRST_PROC; p <= LAST_PROC; p++)
+	// {
+	// 	/* Skip non-ready process. */
+	// 	if (p->state != PROC_READY)
+	// 		continue;
+	//
+	// 	/*
+	// 	 * Process with higher
+	// 	 * waiting time found.
+	// 	 */
+	// 	if (p->counter > next->counter)
+	// 	{
+	// 		next->counter++;
+	// 		next = p;
+	// 	}
+	//
+	// 	/*
+	// 	 * Increment waiting
+	// 	 * time of process.
+	// 	 */
+	// 	else
+	// 		p->counter++;
+	// }
+
+	/* Switch to next process. */
+	next->priority = PRIO_USER;
+	next->state = PROC_RUNNING;
+	next->counter = PROC_QUANTUM;
+	switch_to(next);
+}
